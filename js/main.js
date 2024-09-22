1dcfa086f369b5e5f8c5bc70dfe16707f027ee1e
kuwa <163106848+jkuwa@users.noreply.github.com>
2024-09-22 16:59:41 +0900

    追加：ヘッダー

--- js/main.js
+++ js/main.js
@@ -1,1 +1,60 @@
 'use strict';
+
+$(function() {
+  /* ---------- ハンバーガーメニュー ---------- */
+  $(".js-hamburgerBtn").on('click', function() {
+    $(this).toggleClass('is-open');
+    $(".js-nav").toggleClass('is-open');
+    $("body").toggleClass('is-open');
+    $(".c-cover").toggleClass('is-open');
+
+    if ($(this).text() === 'メニューを開く') {
+      $(".js-menu").text('メニューを閉じる');
+      $(".js-hamburgerBtn").attr('aria-expanded', 'true');
+    } else {
+      $(".js-menu").text('メニューを開く');
+      $(".js-hamburgerBtn").attr('aria-expanded', 'false');
+    }
+  });
+
+  // 最後の項目の後ハンバーガーボタンに戻る
+  $(".js-focusTrap").on('focus', function() {
+    if ( $(".js-hamburgerBtn").hasClass('is-open') ) {
+    $(".js-hamburgerBtn").focus();
+    }
+  });
+  
+  // Escキーでメニュー閉じる
+  $(document).keydown( function(e) {
+    if ( e.which === 27 && $(".js-hamburgerBtn").hasClass('is-open') ) {
+      e.preventDefault();
+      $(".js-hamburgerBtn").removeClass('is-open').attr('aria-expanded', 'false');
+      $(".js-nav").removeClass('is-open');
+      $("body").removeClass('is-open');
+      $(".c-cover").removeClass('is-open');
+      $(".js-menu").text('メニューを開く');
+    }
+  });
+  
+  // リサイズでメニュー閉じる
+  $(window).on('resize', function() {
+    if ( $(".js-hamburgerBtn").hasClass('is-open') ) {
+      $(".js-hamburgerBtn").removeClass('is-open').attr('aria-expanded', 'false');
+      $(".js-nav").removeClass('is-open');
+      $("body").removeClass('is-open');
+      $(".c-cover").removeClass('is-open');
+      $(".js-menu").text('メニューを開く');
+    }
+  });
+  
+  // ページ内リンクでメニューを閉じる
+  $(".js-nav a").on('click', function() {
+    if ( $(".js-hamburgerBtn").hasClass('is-open') ) {
+      $(".js-hamburgerBtn").removeClass('is-open').attr('aria-expanded', 'false');
+      $(".js-nav").removeClass('is-open');
+      $("body").removeClass('is-open');
+      $(".c-cover").removeClass('is-open');
+      $(".js-menu").text('メニューを開く');
+    }
+  });
+})
